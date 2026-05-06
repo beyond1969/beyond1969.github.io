82420fc6e9f8781ab46f47a494ec6c6bd6b0867c
김혁중 <beyond1969@gmail.com>
2026-05-06 22:16:06 +0900

    feat: replace template placeholders with Clukay theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '클루카이의 기록실';
+export const SITE_DESCRIPTION = '전 404 소대 소속 엘리트 용병 클루카이가 관리하는 지휘관 전용 전술/기술 기록실입니다.';
